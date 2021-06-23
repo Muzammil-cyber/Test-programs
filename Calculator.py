1196a949b7fd12d5f657d0a6e0a83338baa191cd
Muzammil Loya <73660510+Muzammil-cyber@users.noreply.github.com>
2021-06-23 21:19:48 +0500

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,18 +1,21 @@
 print("Welcome to Calculator \n Made by Muzammil")
 ans = float(input("First Number: "))
 n2 = float(input("Second Number: "))
+txt = str(ans)
 while n2 != 0:
     op = input("Input operator: ")
     while not op == "+" and op == "-" and op == "*" and op == "x" and op == "/":
         op = input("Please enter the correct operator(+,-,*,/): ")
+
     if op == "+":
         ans += n2
     elif op == "-":
-         ans -= n2
+        ans -= n2
     elif op == "/":
         ans = ans/n2
     else:
         ans *= n2
-    print(ans)
+    txt = txt+" " + op + " " + str(n2)
+    print(txt+ " = "+str(ans))
     n2 = float(input("Another Number: "))
 print("The End!!")
