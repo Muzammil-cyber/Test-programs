cd96d5347065a602212c2e0d66b7a297a478e411
Muzammil Loya <73660510+Muzammil-cyber@users.noreply.github.com>
2021-07-04 16:33:32 +0500

    Using Procedure and Functions
    Take class marks and name as input, output class avg and highest marks and name and lowest marks.

--- Class_Marks2.0.py
+++ Class_Marks2.0.py
@@ -1,4 +1,4 @@
-# Take class marks and name as input, output class avg and highest marks and lowest marks.
+# Take class marks and name as input, output class avg and highest marks and name and lowest marks.
 def enter():
     limit = int(input("How many students are there: "))
     print(limit)
@@ -10,30 +10,31 @@ def enter():
         names[x] = names[x].strip()
         a = names[x]
         names[x] = a[0].upper() + a[1:]
-        txt = "Enter marks of {}, from 0 to {}"
+        txt = "Enter marks of {}, from 0 to {}: "
         marks[x] = int(input(txt.format(names[x], totmarks)))
         while marks[x] > totmarks or marks[x] < 0:
             marks[x] = int(input("Please," + txt.format(names[x], totmarks)))
-    loop(marks, totmarks, limit)
+    loop(names,marks, totmarks, limit)
     print(names, marks)
 
 
-def loop(marks, totmarks, classsize):
+def loop(names,marks, totmarks, classsize):
     total = 0
-    highest = -1
+    highest = [-1, ""]
     lowest = totmarks - 1
     for x in range(classsize):
         totmarks += 1
-        highest = high(marks[x], highest)
+        highest = high(names[x],marks[x], highest[0])
         lowest = low(marks[x], lowest)
         total += marks[x]
-    print("Highest marks are, ", highest, "\nLowest marks are, ", lowest,"\nAverage marks are, ", round(total/classsize))
+    print("Highest marks are,", highest[0], "of " + highest[1] + "\nLowest marks are,", lowest, "\nAverage marks are,", round(total/classsize))
 
 
-def high(mark, highmark):
+def high(name, mark, highmark):
     if mark > highmark:
         highmark = mark
-    return highmark
+        highstd = name
+    return highmark, highstd
 
 
 def low(mark, lowmark):
