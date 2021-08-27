cdb7db658967b9638b6a331ae376a40afffe4dfa
Muzammil Loya <73660510+Muzammil-cyber@users.noreply.github.com>
2021-08-27 13:24:54 +0500

    Create LinkedList.py

---
+++ LinkedList.py
@@ -0,0 +1,30 @@
+fp, hp = 0, -1
+mylist, mylistpointer = [""] * 10, [0] * 10
+for x in range(9):
+    mylistpointer[x] = x + 1
+mylistpointer[9] = hp
+print(mylistpointer)
+
+
+def init(item):
+    global fp, hp
+    if fp == -1:
+        print("List is full")
+    else:
+        mylist[fp] = item
+        hp = fp
+        fp = mylistpointer[fp]
+
+
+def find(item):
+    pointer = 0
+    found = False
+    while not found and pointer != -1:
+        if mylist[pointer] == item:
+            found = True
+        else:
+            pointer = mylistpointer[pointer]
+    if found:
+        print("Item Found")
+    else:
+        print("ERROR 404")
